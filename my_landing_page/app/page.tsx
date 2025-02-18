0600882bb9287d84fd35fef8bf9d50ce411f532d
aminefaouzi39 <126355359+aminefaouzi39@users.noreply.github.com>
2025-02-18 22:42:06 +0100

    Add files via upload

---
+++ my_landing_page/app/page.tsx
@@ -0,0 +1,19 @@
+import CTA from "@/components/sections/cta/default";
+import FAQ from "@/components/sections/faq/default";
+import Hero from "@/components/sections/hero/default";
+import Navbar from "@/components/sections/navbar/default";
+import { Footer } from "@/components/ui/footer";
+
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero/>
+      
+      
+      
+    </>
+
+  );
+}
